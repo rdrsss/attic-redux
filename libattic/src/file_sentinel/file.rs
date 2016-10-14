213b3f82b1ecefae88cdd894d24c7095de538886
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2016-10-13 22:57:24 -0600

    initial commit

---
+++ libattic/src/file_sentinel/file.rs
@@ -0,0 +1,6 @@
+
+
+/// Contains metadata to a file in the file system.
+struct File {
+    Path: String,
+}
---
+++ libattic/src/lib.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
+
+mod file_sentinel {
+    mod file {
+    }
+}
+

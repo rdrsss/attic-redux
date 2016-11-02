a853a8b256094165ec8366ba2df0d2f1df4a852b
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2016-11-02 05:47:32 +0000

    wip

--- libattic/src/file.rs
+++ libattic/src/file.rs
@@ -5,6 +5,7 @@ use crypto::digest::Digest;
 use crypto::sha2::Sha256;
 
 use std;
+use std::io::Read;
 
 pub enum AtticFileError {
     Metadata,
@@ -64,24 +65,48 @@ impl File {
         // Make sure we aren't working with a directory
         if !metadata.is_dir() {
             // Generate hash
-            let res = self.generate_hash(&self.path);
+            let res = self.generate_hash(&self.path, &metadata.len(), &256_u64);
             return Ok(());
         }
         return Err(AtticFileError::IsDirectory);
     }
 
     /// Generate sha256 rolling hash for file.
-    fn generate_hash(&self, path :&String) -> Result<String, AtticFileError> {
+    ///     * path      : path to file
+    ///     * size      : size of file in bytes. 
+    ///     * stride    : number of bytes to read in at a time.
+    fn generate_hash(&self, path :&String, size :&u64, stride :&u64) -> Result<String, AtticFileError> {
         // Open file
         match std::fs::File::open(path) {
             Ok(file) => {
                 // Create buffered reader around file.
-                let mut reader = std::io::BufReader::new(file);
+                let mut reader = &std::io::BufReader::new(file);
                 // Create Sha256 Object
                 let mut hasher = crypto::sha2::Sha256::new();
-                // TODO :: This now
+
                 // Read in chunks until done.
-                // Input to Hasher
+                let mut pos :u64 = 0; 
+                while pos < *size {
+                    // Calculate number of bytes to read.
+                    let mut read;
+                    if (pos + stride) > *size {
+                        read = *size - pos;
+                    } else {
+                        read = *stride;
+                    }
+
+                    // Note* should check or do an assertion if read is over the size 
+                    //       of usize since we are going from u64 possibly down.
+                    let mut buf = Vec::<u8>::with_capacity(read as usize);
+                    {
+                    reader.take(read);
+                    }
+
+                    //file.read_exact(buf);
+
+                    // Input to Hasher
+                }
+
                 // Generate result
             },
             Err(e) => {
